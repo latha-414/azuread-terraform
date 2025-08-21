773fd88360e903bac06a264546487d734193f190
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-21 16:54:18 +0530

    Update main.tf

--- modules/app_registration/main.tf
+++ modules/app_registration/main.tf
@@ -1,10 +1,12 @@
 resource "azuread_application" "this" {
   display_name = var.display_name
   owners       = var.owner_object_ids
+
   api {
     oauth2_permission_scope {
       admin_consent_description  = "Access API as a user"
       admin_consent_display_name = "User API Access"
+      enabled                    = true
       id                         = uuid()
       type                       = "User"
       value                      = "access_as_user"
@@ -12,15 +14,17 @@ resource "azuread_application" "this" {
   }
 }
 
+# Service Principal must reference the application's "client_id"
 resource "azuread_service_principal" "this" {
-  application_id = azuread_application.this.application_id
+  client_id = azuread_application.this.client_id
 }
 
+# App password (client secret)
 resource "azuread_application_password" "client_secret" {
-  application_object_id = azuread_application.this.object_id
-  display_name          = "client-secret"
-  value                 = var.client_secret_value != "" ? var.client_secret_value : random_password.secret.result
-  end_date_relative     = "8760h" # 1 year
+  application_id   = azuread_application.this.id # <-- fixed, use "id" not object_id
+  display_name     = "client-secret"
+  value            = var.client_secret_value != "" ? var.client_secret_value : random_password.secret.result
+  end_date_relative = "8760h" # 1 year
 }
 
 resource "random_password" "secret" {
