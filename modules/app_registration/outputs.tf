55a67692d24f18e09cd9a2ef243a5688b6729c8a
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-21 16:55:06 +0530

    Update outputs.tf

--- modules/app_registration/outputs.tf
+++ modules/app_registration/outputs.tf
@@ -1,15 +1,25 @@
-output "app_id" {
-  description = "Azure AD application ID"
-  value       = azuread_application.this.application_id
+output "application_id" {
+  description = "The unique client ID (applicationId) of the app registration"
+  value       = azuread_application.this.client_id
 }
 
-output "sp_id" {
-  description = "Service principal ID"
-  value       = azuread_service_principal.this.id
+output "application_object_id" {
+  description = "The object ID of the application"
+  value       = azuread_application.this.object_id
 }
 
-output "client_secret" {
-  description = "Client secret value (random or provided)"
-  value       = azuread_application_password.client_secret.value
+output "service_principal_id" {
+  description = "The object ID of the service principal"
+  value       = azuread_service_principal.this.object_id
+}
+
+output "client_secret_value" {
+  description = "The client secret value (only available after apply)"
   sensitive   = true
+  value       = azuread_application_password.client_secret.value
+}
+
+output "client_secret_id" {
+  description = "The ID of the application password (not the secret itself)"
+  value       = azuread_application_password.client_secret.id
 }
