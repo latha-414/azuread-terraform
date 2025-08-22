f099584ac68ac75f43684a43589ae8697e9a8e35
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 11:12:40 +0530

    Update outputs.tf

--- modules/app_registration/outputs.tf
+++ modules/app_registration/outputs.tf
@@ -1,25 +1,14 @@
-output "application_id" {
-  description = "The unique client ID (applicationId) of the app registration"
-  value       = azuread_application.this.client_id
-}
-
-output "application_object_id" {
-  description = "The object ID of the application"
-  value       = azuread_application.this.object_id
+output "app_id" {
+  value = azuread_application.this.application_id
 }
 
 output "service_principal_id" {
-  description = "The object ID of the service principal"
-  value       = azuread_service_principal.this.object_id
+  value = azuread_service_principal.this.id
 }
 
-output "client_secret_value" {
-  description = "The client secret value (only available after apply)"
-  sensitive   = true
-  value       = azuread_application_password.client_secret.value
-}
-
-output "client_secret_id" {
-  description = "The ID of the application password (not the secret itself)"
-  value       = azuread_application_password.client_secret.id
+output "app_role_ids" {
+  description = "Map of app role values to IDs"
+  value = {
+    for r in azuread_application.this.oauth2_permission_scope : r.value => r.id
+  }
 }
