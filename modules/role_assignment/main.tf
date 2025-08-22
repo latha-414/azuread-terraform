e402de309ebe72bf5725e9589d7214372b2d3dfe
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 11:46:43 +0530

    Update main.tf

--- modules/role_assignment/main.tf
+++ modules/role_assignment/main.tf
@@ -1,7 +1,7 @@
 resource "azuread_app_role_assignment" "this" {
   for_each = { for a in var.assignments : "${a.principal_object_id}-${a.resource_object_id}" => a }
 
-  app_role_id         = each.value.app_role_id
-  principal_object_id = each.value.principal_object_id
-  resource_object_id  = each.value.resource_object_id
+  app_role_id          = each.value.app_role_id
+  principal_object_id  = each.value.principal_object_id
+  resource_object_id   = each.value.resource_object_id
 }
