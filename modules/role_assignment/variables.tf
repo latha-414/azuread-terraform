6aa53e4032326a1d30cc58bb6649b24fa40aa25c
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 11:47:08 +0530

    Update variables.tf

--- modules/role_assignment/variables.tf
+++ modules/role_assignment/variables.tf
@@ -1,8 +1,8 @@
 variable "assignments" {
   description = "List of app role assignments"
   type = list(object({
-    app_role_id          = string
-    principal_object_id  = string
-    resource_object_id   = string
+    app_role_id         = string
+    principal_object_id = string
+    resource_object_id  = string
   }))
 }
