b2287669763030fb7a074379de58a4cb81e2e725
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 15:50:54 +0530

    Update outputs.tf

--- modules/app_registration/outputs.tf
+++ modules/app_registration/outputs.tf
@@ -1,8 +1,8 @@
 output "application_id" {
-  value = azuread_application.this.application_id
+  value = azuread_application.this.client_id
 }
 
-output "client_id" {
+output "service_principal_id" {
   value = azuread_service_principal.this.id
 }
 
