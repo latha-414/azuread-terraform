1764e88e8e2ef88feef38d6aa2c1a26061f3597d
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 11:26:57 +0530

    Update outputs.tf

--- modules/role_assignment/outputs.tf
+++ modules/role_assignment/outputs.tf
@@ -1,14 +1,4 @@
-output "app_id" {
-  description = "Azure AD Application ID"
-  value       = azuread_application.this.id
-}
-
-output "service_principal_id" {
-  description = "Service principal ID for the app"
-  value       = azuread_service_principal.this.id
-}
-
-output "app_role_ids" {
-  description = "Map of app role display names to IDs"
-  value = { for r in azuread_application.this.app_role : r.value => r.id }
+# empty or can output assigned IDs
+output "assigned_ids" {
+  value = [for r in azuread_app_role_assignment.this : r.id]
 }
