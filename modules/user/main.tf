c6f914bcd4c72f4892b17d89ad44524baba997ca
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 15:41:00 +0530

    Update main.tf

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -1,9 +1,7 @@
 resource "azuread_user" "this" {
-  for_each = { for u in var.users : u.user_principal_name => u }
-
-  display_name       = each.value.display_name
-  user_principal_name = each.value.user_principal_name
-  mail_nickname      = each.value.mail_nickname
-  password           = each.value.password
+  user_principal_name = var.user_principal_name
+  display_name        = var.display_name
+  mail_nickname       = var.mail_nickname
+  password            = var.password
   force_password_change = false
 }
