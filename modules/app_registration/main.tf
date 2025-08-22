4b35ad7b995bc0ac7594dd4b00332d9ee9669964
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 15:40:00 +0530

    Update main.tf

--- modules/app_registration/main.tf
+++ modules/app_registration/main.tf
@@ -1,22 +1,13 @@
 resource "azuread_application" "this" {
   display_name = var.display_name
-  owners       = var.owner_object_ids
 }
 
 resource "azuread_service_principal" "this" {
-  application_id = azuread_application.this.id
+  application_id = azuread_application.this.application_id
 }
 
 resource "azuread_application_password" "client_secret" {
   application_id = azuread_application.this.id
   display_name   = "client-secret"
   end_date       = timeadd(timestamp(), "8760h") # 1 year
-}
-
-resource "azuread_app_role" "access_as_user" {
-  application_object_id = azuread_application.this.id
-  display_name          = "access_as_user"
-  allowed_member_types  = ["User", "Application"]
-  description           = "Access API as a user"
-  value                 = "access_as_user"
 }
