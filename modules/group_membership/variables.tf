d1a03bcdf7a741efd2f10d5a51cb9f7ed8f7cb41
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 17:42:23 +0530

    Create variables.tf

---
+++ modules/group_membership/variables.tf
@@ -0,0 +1,9 @@
+variable "group_object_id" {
+  type        = string
+  description = "The Object ID of the group"
+}
+
+variable "member_object_id" {
+  type        = string
+  description = "The Object ID of the user/service principal to add to the group"
+}
