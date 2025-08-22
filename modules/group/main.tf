e4cc179c760bf53d19a70fdd72baed16c0cb08fe
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 15:42:13 +0530

    Update main.tf

--- modules/group/main.tf
+++ modules/group/main.tf
@@ -1,14 +1,5 @@
 resource "azuread_group" "this" {
-  for_each = { for g in var.groups : g.display_name => g }
-
-  display_name    = each.value.display_name
-  mail_nickname   = each.value.mail_nickname
+  display_name     = var.display_name
+  mail_enabled     = false
   security_enabled = true
-}
-
-resource "azuread_group_member" "membership" {
-  for_each = { for m in var.memberships : "${m.user_upn}-${m.group_name}" => m }
-
-  group_object_id  = azuread_group.this[each.value.group_name].object_id
-  member_object_id = var.user_object_ids[each.value.user_upn]
 }
