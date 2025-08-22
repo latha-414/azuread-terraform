0dd38b1a190767fe8212b0eb8fb0a345507a0cf7
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-22 11:15:53 +0530

    Update main.tf

--- modules/app_registration/main.tf
+++ modules/app_registration/main.tf
@@ -1,25 +1,24 @@
 resource "azuread_application" "this" {
   display_name = var.display_name
   owners       = var.owner_object_ids
-
-  api {
-    oauth2_permission_scope {
-      admin_consent_description  = "Access API as a user"
-      admin_consent_display_name = "User API Access"
-      enabled                    = true
-      id                         = uuid()
-      type                       = "User"
-      value                      = "access_as_user"
-    }
-  }
 }
 
 resource "azuread_service_principal" "this" {
-  application_id = azuread_application.this.application_id
+  # v2+ uses `application_id` as client reference
+  application_id = azuread_application.this.id
 }
 
 resource "azuread_application_password" "client_secret" {
-  application_id = azuread_application.this.application_id
+  application_id = azuread_application.this.id
   display_name   = "client-secret"
   end_date       = timeadd(timestamp(), "8760h")
+}
+
+# Optional: app roles
+resource "azuread_app_role" "access_as_user" {
+  application_object_id = azuread_application.this.id
+  display_name          = "access_as_user"
+  allowed_member_types  = ["User", "Application"]
+  description           = "Access API as a user"
+  value                 = "access_as_user"
 }
