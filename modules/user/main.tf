605d6d48831a88401a4ed556a4a96718f4226fac
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:12:38 +0530

    Update main.tf

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -1,7 +1,8 @@
-resource "azuread_user" "this" {
+resource "azuread_user" "user" {
   user_principal_name = var.user_principal_name
   display_name        = var.display_name
-  mail_nickname       = var.mail_nickname
-  password            = var.password
+  mail_nickname       = replace(var.user_principal_name, "@.*", "")
+  password            = "TempP@ssword123" # Ideally generate or pass securely
   force_password_change = false
+  account_enabled      = true
 }
