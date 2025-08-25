5e6938def71b16c5460bab13bcdd15a519839863
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 14:34:31 +0530

    Update outputs.tf

--- modules/app_registration/outputs.tf
+++ modules/app_registration/outputs.tf
@@ -1,12 +1,11 @@
-output "application_id" {
-  value = azuread_application.this.client_id
-}
-
-output "service_principal_object_id" {
+output "service_principal_id" {
   value = azuread_service_principal.this.object_id
 }
 
-output "client_secret" {
-  value     = azuread_application_password.client_secret.value
-  sensitive = true
+output "application_id" {
+  value = azuread_application.this.application_id
+}
+
+output "admin_app_role_id" {
+  value = azuread_application.this.app_roles[0].id
 }
