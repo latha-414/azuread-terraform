fa906cdba510d44a972e3c768195df6c7c9ec530
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:13:10 +0530

    Update variables.tf

--- modules/group_membership/variables.tf
+++ modules/group_membership/variables.tf
@@ -1,9 +1,9 @@
-variable "group_object_id" {
-  type        = string
-  description = "The Object ID of the group"
+variable "environment" {
+  type = string
 }
-
-variable "member_object_id" {
-  type        = string
-  description = "The Object ID of the user/service principal to add to the group"
+variable "user_object_id" {
+  type = string
+}
+variable "group_object_id" {
+  type = string
 }
