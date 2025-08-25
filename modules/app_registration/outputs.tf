4c9545c87d8a47157eeced9b0386d510dd0dc869
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 14:52:05 +0530

    Update outputs.tf

--- modules/app_registration/outputs.tf
+++ modules/app_registration/outputs.tf
@@ -1,11 +1,12 @@
+# Outputs
 output "service_principal_id" {
   value = azuread_service_principal.this.object_id
 }
 
 output "application_id" {
-  value = azuread_application.this.application_id
+  value = azuread_application.this.id
 }
 
 output "admin_app_role_id" {
-  value = azuread_application.this.app_role[0].id
+  value = tolist(azuread_application.this.app_role)[0].id
 }
