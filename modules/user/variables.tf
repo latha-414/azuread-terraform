ec4b2f253a8d942478b77eb114946c3940b6da72
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 17:45:22 +0530

    Update variables.tf

--- modules/user/variables.tf
+++ modules/user/variables.tf
@@ -1,12 +1,32 @@
-variable "environment" {
-  type = string
-}
-variable "location" {
-  type = string
-}
 variable "user_principal_name" {
-  type = string
+  type        = string
+  description = "The UPN (email) of the Azure AD user."
 }
+
 variable "display_name" {
-  type = string
+  type        = string
+  description = "The display name of the Azure AD user."
+}
+
+variable "mail_nickname" {
+  type        = string
+  description = "The mail nickname (alias) for the Azure AD user."
+}
+
+variable "user_password" {
+  type        = string
+  sensitive   = true
+  description = "The password for the Azure AD user."
+}
+
+variable "force_password_change" {
+  type        = bool
+  default     = true
+  description = "Whether to force the user to change password on first login."
+}
+
+variable "account_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether the user account is enabled."
 }
