177cbad47dffb1267976ace8227ca956df409cc4
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:14:16 +0530

    Update main.tf

--- modules/role_assignment/main.tf
+++ modules/role_assignment/main.tf
@@ -1,5 +1,5 @@
-resource "azuread_app_role_assignment" "this" {
-  principal_object_id = var.principal_object_id
-  resource_object_id  = var.resource_object_id
-  app_role_id         = var.app_role_id
+resource "azurerm_role_assignment" "role_assignment" {
+  principal_id         = var.principal_id
+  role_definition_name = var.role_definition_name
+  scope                = var.scope
 }
