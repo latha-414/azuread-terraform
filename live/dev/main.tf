d928afc60f8bbf8cec9204a130cdee7ec0d3e012
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:07:26 +0530

    Update main.tf

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -1,35 +1,54 @@
 provider "azuread" {}
 
+module "key_vault" {
+  source              = "../modules/key_vault"
+  environment         = var.environment
+  location            = var.location
+  resource_group_name = "my-resource-group"
+  key_vault_name      = "kv-${var.environment}-project"
+  tenant_id           = data.azuread_client_config.current.tenant_id
+}
+
 module "app_registration" {
-  source       = "../../modules/app_registration"
-  display_name = var.app_display_name
+  source               = "../modules/app_registration"
+  environment          = var.environment
+  location             = var.location
+  app_name             = "my-app"
+  key_vault_id         = module.key_vault.key_vault_id
+  key_vault_secret_name = "app-sp-password-dev"  # name of secret in key vault
+}
+
+data "azuread_client_config" "current" {}
+
+module "group" {
+  source      = "../modules/group"
+  environment = var.environment
+  location    = var.location
+  group_name  = "my-group"
 }
 
 module "user" {
-  source              = "../../modules/user"
-  user_principal_name = var.user_principal_name
-  display_name        = "Dev User"
-  mail_nickname       = "devuser"
-  password            = var.user_password
-}
-
-module "group" {
-  source       = "../../modules/group"
-  display_name = var.group_name
+  source      = "../modules/user"
+  environment = var.environment
+  location    = var.location
+  user_principal_name = "user1@domain.com"
+  display_name        = "User One"
 }
 
 module "group_membership" {
-  source           = "../../modules/group_membership"
-  group_object_id  = module.group.group_id
-  member_object_id = module.user.user_id
+  source       = "../modules/group_membership"
+  environment  = var.environment
+  user_object_id = module.user.user_object_id
+  group_object_id = module.group.group_object_id
 }
 
 module "role_assignment" {
-  source              = "../../modules/role_assignment"
-  principal_object_id = module.user.user_id
-  resource_object_id  = module.app_registration.service_principal_id
-  app_role_id         = module.app_registration.admin_app_role_id
+  source            = "../modules/role_assignment"
+  environment       = var.environment
+  principal_id      = module.user.user_object_id
+  role_definition_name = "Contributor"
+  scope             = "/subscriptions/your-subscription-id"
 }
 
-
+  
 
