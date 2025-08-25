4c953ed678694830ac0b8a7c454d377b32c29dfe
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 17:45:42 +0530

    Update outputs.tf

--- modules/user/outputs.tf
+++ modules/user/outputs.tf
@@ -1,3 +1,9 @@
-output "user_object_id" {
-  value = azuread_user.user.object_id
+output "user_id" {
+  description = "The object ID of the created Azure AD user."
+  value       = azuread_user.user.object_id
+}
+
+output "user_principal_name" {
+  description = "The user principal name (email) of the Azure AD user."
+  value       = azuread_user.user.user_principal_name
 }
