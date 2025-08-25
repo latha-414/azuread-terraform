3eea46f106d894f7487803cb58e0109239e0911c
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:21:36 +0530

    Update main.tf

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -1,7 +1,7 @@
 provider "azuread" {}
 
 module "key_vault" {
-  source              = "../modules/key_vault"
+  source              = "../../modules/key_vault"
   environment         = var.environment
   location            = var.location
   resource_group_name = "my-resource-group"
@@ -10,25 +10,23 @@ module "key_vault" {
 }
 
 module "app_registration" {
-  source               = "../modules/app_registration"
+  source               = "../../modules/app_registration"
   environment          = var.environment
   location             = var.location
   app_name             = "my-app"
   key_vault_id         = module.key_vault.key_vault_id
-  key_vault_secret_name = "app-sp-password-dev"  # name of secret in key vault
+  key_vault_secret_name = "app-sp-password-dev"
 }
 
-data "azuread_client_config" "current" {}
-
 module "group" {
-  source      = "../modules/group"
+  source      = "../../modules/group"
   environment = var.environment
   location    = var.location
   group_name  = "my-group"
 }
 
 module "user" {
-  source      = "../modules/user"
+  source      = "../../modules/user"
   environment = var.environment
   location    = var.location
   user_principal_name = "user1@domain.com"
@@ -36,19 +34,17 @@ module "user" {
 }
 
 module "group_membership" {
-  source       = "../modules/group_membership"
+  source       = "../../modules/group_membership"
   environment  = var.environment
   user_object_id = module.user.user_object_id
   group_object_id = module.group.group_object_id
 }
 
 module "role_assignment" {
-  source            = "../modules/role_assignment"
+  source            = "../../modules/role_assignment"
   environment       = var.environment
   principal_id      = module.user.user_object_id
   role_definition_name = "Contributor"
   scope             = "/subscriptions/your-subscription-id"
 }
-
-  
 
