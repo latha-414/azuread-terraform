4193050015dc4255336e1d42a105924c0311bc12
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 17:30:04 +0530

    Update main.tf

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -1,9 +1,17 @@
+resource "random_id" "unique_hex" {
+  byte_length = 4
+}
+
+locals {
+  key_vault_name = "${var.key_vault_base_name}-${var.environment}-${substr(random_id.unique_hex.hex, 0, 6)}"
+}
+
 resource "azurerm_key_vault" "this" {
-  name                        = var.key_vault_name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                   = "standard"
+  name                = local.key_vault_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
@@ -22,6 +30,3 @@ resource "azurerm_key_vault" "this" {
     ]
   }
 }
-
-
-data "azuread_client_config" "current" {}
