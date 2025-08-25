11c96636874626f30c150d0d7ab3e76485e364cb
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 14:43:04 +0530

    Update main.tf

--- modules/app_registration/main.tf
+++ modules/app_registration/main.tf
@@ -1,17 +1,15 @@
+# Create Azure AD Application
 resource "azuread_application" "this" {
   display_name = var.display_name
 
-  app_roles = [
-    {
-      allowed_member_types = ["User"]
-      description          = "Admin role for the app"
-      display_name         = "Admin"
-      is_enabled           = true
-      value                = "Admin"
-    }
-  ]
+  api {
+    requested_access_token_version = 2
+  }
 }
 
+# Create Service Principal for the Application
 resource "azuread_service_principal" "this" {
   application_id = azuread_application.this.application_id
+  # client_id is required for AzureAD provider >= 3.0.0
+  client_id      = azuread_application.this.application_id
 }
