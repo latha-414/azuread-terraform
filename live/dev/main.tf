e2490d5e1395494852bc3d3f87b2b0be31bb98c2
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 14:21:49 +0530

    Update main.tf

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -26,8 +26,9 @@ module "group_membership" {
 
 module "role_assignment" {
   source              = "../../modules/role_assignment"
-  principal_object_id = module.app_registration.service_principal_object_id
-  resource_object_id  = var.resource_object_id
-  app_role_id         = var.app_role_id
+  principal_object_id = module.user.user_id
+  resource_object_id  = module.application.app_id
+  app_role_id         = module.application.app_role_id
 }
+
 
