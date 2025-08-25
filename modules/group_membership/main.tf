37712c6096640331d1586ff0606fbfc858ea887c
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:13:26 +0530

    Update main.tf

--- modules/group_membership/main.tf
+++ modules/group_membership/main.tf
@@ -1,4 +1,4 @@
-resource "azuread_group_member" "this" {
+resource "azuread_group_member" "group_member" {
   group_object_id  = var.group_object_id
-  member_object_id = var.member_object_id
+  member_object_id = var.user_object_id
 }
