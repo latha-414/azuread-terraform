01b0c3be77e27739f229024dfa242d28b8eeaa90
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 16:59:15 +0530

    Update main.tf

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -4,25 +4,24 @@ resource "azurerm_key_vault" "this" {
   resource_group_name         = var.resource_group_name
   tenant_id                   = var.tenant_id
   sku_name                   = "standard"
-
-  # soft_delete_enabled removed
-
-  # Optional
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   access_policy {
-  tenant_id = var.tenant_id
-  object_id = data.azuread_client_config.current.object_id
-  key_permissions = [
-    "Get",
-    "List",
-  ]
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-  ]
+    tenant_id = var.tenant_id
+    object_id = data.azuread_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+      "List",
+    ]
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+    ]
+  }
 }
+
 
 data "azuread_client_config" "current" {}
