80fdcbc2d18e5aec3ea3dab638ce199498b50c41
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 10:47:55 +0530

    Update variables.tf

--- modules/role_assignment/variables.tf
+++ modules/role_assignment/variables.tf
@@ -1,3 +1,14 @@
-variable "principal_object_id" { type = string }
-variable "resource_object_id" { type = string }
-variable "app_role_id" { type = string }
+variable "principal_object_id" {
+  type = string
+  description = "Object ID of the Azure AD Service Principal"
+}
+
+variable "resource_object_id" {
+  type = string
+  description = "Object ID of the target resource"
+}
+
+variable "app_role_id" {
+  type = string
+  description = "App role ID to assign"
+}
