ba161d0b17a13167fa0d18f24769dc7dd4a28a12
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 15:15:10 +0530

    Create variables.tf

---
+++ modules/key_vault/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "key_vault_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
