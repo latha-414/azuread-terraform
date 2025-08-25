8714b5f4a2958f6c9d29d3bd89e8a83f2b7af213
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-25 10:48:19 +0530

    Update main.tf

--- modules/role_assignment/main.tf
+++ modules/role_assignment/main.tf
@@ -1,5 +1,5 @@
 resource "azuread_app_role_assignment" "this" {
-  principal_object_id = azuread_service_principal.this.object_id
+  principal_object_id = var.principal_object_id
   resource_object_id  = var.resource_object_id
   app_role_id         = var.app_role_id
 }
