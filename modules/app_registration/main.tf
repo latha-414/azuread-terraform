2228f16e93a530795b9e9a51f19046e8452be672
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-26 11:39:09 +0530

    Update main.tf

--- modules/app_registration/main.tf
+++ modules/app_registration/main.tf
@@ -6,13 +6,19 @@ resource "azuread_service_principal" "app_sp" {
   client_id = azuread_application.app.client_id
 }
 
-data "azurerm_key_vault_secret" "app_sp_password" {
-  name         = var.key_vault_secret_name
-  key_vault_id = var.key_vault_id
-}
-
 resource "azuread_application_password" "app_sp_password" {
   application_id = azuread_application.app.id
   display_name   = "Secret for ${var.app_name}-${var.environment}"
   end_date      = timeadd(timestamp(), "8760h")  # 1 year from now
+}
+
+resource "azurerm_key_vault_secret" "app_sp_password_secret" {
+  name         = var.key_vault_secret_name  # e.g., "app-sp-password-dev"
+  value        = azuread_application_password.app_sp_password.value
+  key_vault_id = var.key_vault_id
+}
+
+data "azurerm_key_vault_secret" "app_sp_password" {
+  name         = var.key_vault_secret_name
+  key_vault_id = var.key_vault_id
 }
