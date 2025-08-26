6f6166d87b66149015f349d9be87351b54abf4db
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-26 10:33:24 +0530

    Update main.tf

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -2,7 +2,7 @@ resource "azuread_user" "user" {
   user_principal_name   = var.user_principal_name
   display_name          = var.display_name
   mail_nickname         = var.mail_nickname
-  user_password         = var.user_password
+  password              = var.user_password
   force_password_change = var.force_password_change
   account_enabled       = var.account_enabled
 }
